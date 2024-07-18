872f722f7031ebc54f3f7b8a384324c86d09f20b
anhdtph32085 <152075036+anhdtph32085@users.noreply.github.com>
2024-07-18 18:25:01 +0700

    18.07

---
+++ factories/ProductCommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductComment>
+ */
+class ProductCommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment' => $this->faker->sentence,
+            'userId'=>User::all()->random()->id,
+            'productId'=>Product::all()->random()->id,
+
+            'created_at' => $this->faker->dateTime,
+            'updated_at' => $this->faker->dateTime,
+        ];
+    }
+}
